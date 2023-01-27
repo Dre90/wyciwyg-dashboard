6f3a5f4a0c4d47635919812e5b8c1248b5b65181
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2023-01-27 15:54:57 +0100

    byttet til å bruke Googel og Github login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     extend: {
       colors: {
         "bv-dark-gray": "#1e1e1e",
+        "dark-gray-plus1": "#282828",
+        "dark-gray-plus2": "#323232",
+        "dark-gray-plus3": "#3c3c3c",
+        "dark-gray-plus4": "#4b4b4b",
         "bv-text-color": "#eeeeee",
         "bv-green": "#79fe9d",
         "bv-green-hover-bg": "#313833",
