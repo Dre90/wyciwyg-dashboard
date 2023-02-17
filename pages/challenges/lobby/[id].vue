9adc452faa56c820a8f336023058aed770ec02f8
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2023-02-17 14:25:04 +0100

    fungerene lobby side

--- pages/challenges/lobby/[id].vue
+++ pages/challenges/lobby/[id].vue
@@ -4,8 +4,18 @@
       <NuxtLink :to="`/challenges/${id}`" class="">Back</NuxtLink>
     </div>
 
-    <div class="flex gap-10">
-      <div class="flex w-1/4 flex-col">Deltaker liste</div>
+    <div class="mt-10 flex gap-10">
+      <div class="flex w-1/4 flex-col">
+        <ul>
+          <li
+            v-for="item in userList"
+            :key="item.name"
+            class="pb-3 text-2xl text-bv-green"
+          >
+            {{ item.name }} - {{ item.combo }}
+          </li>
+        </ul>
+      </div>
       <div class="flex w-1/2 flex-col">
         <div v-if="!gameStarted" class="flex flex-col items-center">
           <span class="text-9xl text-bv-green">{{ gamePin }}</span>
@@ -42,6 +52,13 @@
             Start!
           </button>
         </div>
+        <div
+          v-if="gameStarted"
+          class="flex flex-col items-center text-bv-green"
+        >
+          <div class="mb-6 text-9xl">{{ highestCombo }}</div>
+          <div class="text-xl">Highest combo</div>
+        </div>
       </div>
     </div>
   </div>
@@ -49,13 +66,14 @@
 
 <script setup>
 const { id } = useRoute().params;
-const client = useSupabaseClient();
+const supabase = useSupabaseClient();
 const user = useSupabaseUser();
 const gamePin = ref("");
 const gameStarted = ref(false);
 const minutes = ref(15);
 const timerInterval = ref("");
 let counter = ref(0);
+const userList = ref([]);
 
 const formatTime = (timer) => {
   const minutes = Math.floor(timer / 60);
@@ -82,7 +100,7 @@ const startTimer = () => {
   }, 1000);
 };
 
-const { data, error } = await client
+const { data, error } = await supabase
   .from("Challenges")
   .select("game_pin")
   .eq("author_id", user.value.id)
@@ -93,6 +111,71 @@ const { data, error } = await client
 if (error) console.log(error.message);
 
 if (data) gamePin.value = data.game_pin;
+
+const highestCombo = computed(() => {
+  return userList.value.reduce((acc, curr) => {
+    if (curr.combo > acc) {
+      return curr.combo;
+    }
+    return acc;
+  }, 0);
+});
+
+onMounted(async () => {
+  let existingData = [];
+  const { data, error } = await supabase
+    .from("Stats")
+    .select("*")
+    .eq("challenge_id", id);
+
+  // TODO: error handling
+  if (error) console.log(error.message);
+  if (data) existingData = data;
+
+  const initialUserList = existingData.map((i) => {
+    return {
+      name: i.name,
+      combo: 0,
+    };
+  });
+
+  userList.value = initialUserList;
+
+  supabase
+    .channel("*")
+    .on(
+      "postgres_changes",
+      { event: "*", schema: "*", table: "Stats" },
+      (payload) => {
+        if (payload.eventType == "INSERT") {
+          // Add name.
+          userList.value.push({
+            name: payload.new.name,
+            combo: 0,
+          });
+        }
+
+        if (payload.new?.challenge_id == id) {
+          const user = payload.new.name;
+          const value = parseInt(payload.new.value, 10);
+
+          const updatedUserList = userList.value.map((i) => {
+            if (i.name !== user) {
+              return i;
+            }
+
+            return {
+              name: user,
+              combo: value,
+            };
+          });
+
+          userList.value = updatedUserList.sort((a, b) => a.combo < b.combo);
+        }
+      }
+    )
+    .subscribe();
+});
 
 definePageMeta({
   middleware: "auth",
