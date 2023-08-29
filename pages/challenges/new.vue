59ce5f18972f8725b26618db5a9955759259ab42
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2023-08-29 15:59:06 +0200

    tekst fungere

--- pages/challenges/new.vue
+++ pages/challenges/new.vue
@@ -6,7 +6,7 @@
         class="flex max-w-xl flex-col gap-4"
         @submit.prevent="createChallenge"
       >
-        <div class="flex flex-col gap-1">
+        <fieldset class="flex flex-col gap-1">
           <label for="name">Name</label>
           <input
             v-model="name"
@@ -14,39 +14,36 @@
             type="text"
             placeholder=""
             name="name"
-            class="text-sm text-bv-dark-gray"
+            class="rounded-md text-sm text-bv-dark-gray"
           />
-        </div>
+        </fieldset>
 
-        <div class="flex flex-col gap-1">
+        <fieldset class="flex flex-col gap-1">
           <label for="instructions">Instructions</label>
           <textarea
             v-model="instructions"
             rows="16"
             name="instructions"
-            class="text-sm text-bv-dark-gray"
+            class="rounded-md text-sm text-bv-dark-gray"
           />
-        </div>
+        </fieldset>
 
-        <div class="flex flex-col gap-1">
+        <!-- <fieldset class="flex flex-col gap-1">
           <label for="refImage">Reference image</label>
           <input
             type="file"
             name="refImage"
-            class="text-sm text-bv-dark-gray"
+            class="rounded-md text-sm text-bv-dark-gray"
+          />
+        </fieldset> -->
+        <div>
+          <input
+            type="submit"
+            class="orangeButton"
+            :value="loading ? 'Loading ...' : 'Save'"
+            :disabled="loading"
           />
         </div>
-
-        <!-- <div class="flex flex-col gap-1">
-          <label for="active">Active</label>
-          <input
-            v-model="active"
-            type="checkbox"
-            placeholder=""
-            name="active"
-            class="text-sm text-bv-dark-gray"
-          />
-        </div> -->
       </form>
     </div>
   </div>
@@ -59,14 +56,15 @@
 import { customAlphabet } from "nanoid";
 const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const nanoid = customAlphabet(alphabet, 6);
+const supabase = useSupabaseClient();
 
 const user = useSupabaseUser();
 
+const loading = ref(false);
 const name = ref("");
 const instructions = ref("");
 const gamePin = ref("");
 const author_id = ref("");
-/* const active = ref(false); */
 
 const getAuthorId = async () => {
   author_id.value = user.value.id;
@@ -81,9 +79,28 @@ onMounted(async () => {
   createGamePin();
 });
 
-const createChallenge = async () => {
-  console.log("Login");
-};
+async function createChallenge() {
+  try {
+    loading.value = true;
+
+    const challenge = {
+      name: name.value,
+      instructions: instructions.value,
+      game_pin: gamePin.value,
+      author_id: author_id.value,
+    };
+
+    let { error } = await supabase.from("Challenges").insert(challenge, {
+      returning: "minimal", // Don't return the value after inserting
+    });
+    if (error) throw error;
+    navigateTo("/challenges");
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    loading.value = false;
+  }
+}
 
 useHead({
   title: "Create new challenge | WYCIWYG",
