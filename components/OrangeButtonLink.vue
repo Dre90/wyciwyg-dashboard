907673194d7437773d8ea2331e6c3e78feb554d0
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2023-08-29 11:30:31 +0200

    OrangeButtonLink component

---
+++ components/OrangeButtonLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink
+    :to="`${link}`"
+    class="inline-flex w-full cursor-pointer items-center justify-center border-2 border-bv-orange bg-bv-orange p-4 text-lg text-bv-text-color-dark transition-all hover:bg-bv-dark-orange focus:bg-bv-dark-orange active:bg-bv-text-color-dark active:fill-bv-orange active:text-bv-orange"
+  >
+    {{ buttonText }}
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+type PropsType = {
+  link: String;
+  buttonText: String;
+};
+
+const { link, buttonText } = defineProps<PropsType>();
+</script>
--- pages/challenges/[id].vue
+++ pages/challenges/[id].vue
@@ -21,36 +21,26 @@
           <p class="text-xl">Game pin</p>
         </div>
         <div class="flex flex-col gap-6">
-          <NuxtLink
-            :to="`lobby/${challenge.id}`"
-            class="inline-flex w-full cursor-pointer items-center justify-center border-2 border-bv-orange bg-bv-orange p-4 text-lg text-bv-text-color-dark transition-all hover:bg-bv-dark-orange focus:bg-bv-dark-orange active:bg-bv-text-color-dark active:fill-bv-orange active:text-bv-orange"
-          >
-            Start
-          </NuxtLink>
-          <NuxtLink
-            :to="`results/${challenge.id}`"
-            class="inline-flex w-full cursor-pointer items-center justify-center border-2 border-bv-orange bg-bv-orange p-4 text-lg text-bv-text-color-dark transition-all hover:bg-bv-dark-orange focus:bg-bv-dark-orange active:bg-bv-text-color-dark active:fill-bv-orange active:text-bv-orange"
-          >
-            Result
-          </NuxtLink>
-          <NuxtLink
-            :to="`voting/${challenge.id}`"
-            class="inline-flex w-full cursor-pointer items-center justify-center border-2 border-bv-orange bg-bv-orange p-4 text-lg text-bv-text-color-dark transition-all hover:bg-bv-dark-orange focus:bg-bv-dark-orange active:bg-bv-text-color-dark active:fill-bv-orange active:text-bv-orange"
-          >
-            Voting information
-          </NuxtLink>
-          <NuxtLink
-            :to="`podium/${challenge.id}`"
-            class="inline-flex w-full cursor-pointer items-center justify-center border-2 border-bv-orange bg-bv-orange p-4 text-lg text-bv-text-color-dark transition-all hover:bg-bv-dark-orange focus:bg-bv-dark-orange active:bg-bv-text-color-dark active:fill-bv-orange active:text-bv-orange"
-          >
-            Podium
-          </NuxtLink>
-          <NuxtLink
-            :to="`votes/${challenge.id}`"
-            class="inline-flex w-full cursor-pointer items-center justify-center border-2 border-bv-orange bg-bv-orange p-4 text-lg text-bv-text-color-dark transition-all hover:bg-bv-dark-orange focus:bg-bv-dark-orange active:bg-bv-text-color-dark active:fill-bv-orange active:text-bv-orange"
-          >
-            All votes
-          </NuxtLink>
+          <OrangeButtonLink
+            :link="`lobby/${challenge.id}`"
+            button-text="Start"
+          />
+          <OrangeButtonLink
+            :link="`results/${challenge.id}`"
+            button-text="Result"
+          />
+          <OrangeButtonLink
+            :link="`voting/${challenge.id}`"
+            button-text="Voting information"
+          />
+          <OrangeButtonLink
+            :link="`podium/${challenge.id}`"
+            button-text="Podium"
+          />
+          <OrangeButtonLink
+            :link="`votes/${challenge.id}`"
+            button-text="All votes"
+          />
         </div>
       </div>
       <div class="">
